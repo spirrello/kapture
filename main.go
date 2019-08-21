ea52c704b233402bf35a11cec90b6ab21b528c71
Stefano Pirrello <spirrello@liaison.com>
2019-08-20 23:10:41 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	//"io/ioutil"
+	"github.com/gorilla/mux"
+)
+
+//Pods struct to collect deployment pods
+type Pods struct {
+	Deployment string `json:"deployment"`
+	Namespace  string `json:"namespace"`
+}
+
+//LogFormat struct for return log messages in json format
+type LogFormat struct {
+	Loglevel string `json:"level"`
+	Message  string `json:"message"`
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("200 OK")
+}
+
+func logMessage(level string, message string) {
+	var logContent LogFormat
+
+	json.Unmarshal([]byte(message), &logContent)
+
+	log.Println(logContent)
+}
+
+func pods(w http.ResponseWriter, r *http.Request) {
+	var pod Pods
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		json.NewEncoder(w).Encode("error reading body")
+	}
+
+	json.Unmarshal(reqBody, &pod)
+
+	json.NewEncoder(w).Encode(pod)
+
+}
+
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/v1/healthcheck", healthCheck)
+	router.HandleFunc("/v1/pods", pods).Methods("POST")
+	log.Fatal(http.ListenAndServe(":9090", router))
+}
