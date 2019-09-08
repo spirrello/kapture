ebb1d64a02c4fccd03c8df73962f7b53cbbd67cf
Stefano Pirrello <spirrello@liaison.com>
2019-09-08 08:14:15 -0400

    test pod list

--- main.go
+++ main.go
@@ -55,7 +55,16 @@ func main() {
 		// - Use helper functions like e.g. errors.IsNotFound()
 		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
 		//_, err = clientset.CoreV1().Pods("default").Get("example-xxxxx", metav1.GetOptions{})
-		_, err = clientset.CoreV1().Pods("kube-system").Get("", "app=nfs-client-provisioner")
+		pods, err = clientset.CoreV1().Pods("kube-system").List(metav1.ListOptions{FieldSelector: "metadata.labels=nfs-client-provisioner"})
+		for _, pod := range pods.Items {
+			fmt.Println(pod.Name, pod.Status)
+		}
+		//NEED TO FIX THIS VAR.
+		// var labelOptions = kubernetes.CoreV1().Pods.GetOptions
+		// appLabel := []byte(`{"app": "nfs-client-provisioner"}`)
+		// err := json.Unmarshal(appLabel, &labelOptions)
+		// _, err = clientset.CoreV1().Pods("kube-system").Get("", metav1.GetOptions{labelOptions, "0"})
+
 		if errors.IsNotFound(err) {
 			fmt.Printf("Pod not found\n")
 		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
