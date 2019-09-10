efc3a5f090f582de75846e3dac4bedafb77fb0a5
Stefano Pirrello <spirrello@liaison.com>
2019-09-09 23:53:35 -0400

    internal and external client configuration

--- main.go
+++ main.go
@@ -15,66 +15,84 @@ limitations under the License.
 package main
 
 import (
+	"flag"
 	"fmt"
-	"time"
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	//
-	// Uncomment to load all auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth
-	//
-	// Or uncomment to load specific auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
-func main() {
-	// creates the in-cluster config
-	config, err := rest.InClusterConfig()
+//externalKubeClient creates the external cluster config
+func externalKubeClient(kubeconfig string) (*kubernetes.Clientset, error) {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
-	// creates the clientset
+
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
+		return nil, err
+	}
+
+	return clientset, nil
+
+}
+
+//internalKubeClient creates the in-cluster config
+func internalKubeClient() (*kubernetes.Clientset, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return clientset, nil
+}
+
+func fetchPod(clientset *kubernetes.Clientset) {
+
+	pods, err := clientset.CoreV1().Pods("kube-system").List(metav1.ListOptions{LabelSelector: "app=nfs-client-provisioner"})
+	for _, pod := range pods.Items {
+		fmt.Println(pod.Name, pod.Spec.NodeName)
+	}
+
+	if errors.IsNotFound(err) {
+		fmt.Printf("Pod not found\n")
+	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+		fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+	} else if err != nil {
 		panic(err.Error())
 	}
-	for {
-		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+
+}
+
+func main() {
+
+	//flag for external client
+	var kubeconfig *string
+	kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+
+	flag.Parse()
+
+	if *kubeconfig != "" {
+		clientset, err := externalKubeClient(*kubeconfig)
 		if err != nil {
 			panic(err.Error())
 		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
-		// Examples for error handling:
-		// - Use helper functions like e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		//_, err = clientset.CoreV1().Pods("default").Get("example-xxxxx", metav1.GetOptions{})
-		pods, err = clientset.CoreV1().Pods("kube-system").List(metav1.ListOptions{LabelSelector: "app=nfs-client-provisioner"})
-		for _, pod := range pods.Items {
-			fmt.Println(pod)
-		}
-		//NEED TO FIX THIS VAR.
-		// var labelOptions = kubernetes.CoreV1().Pods.GetOptions
-		// appLabel := []byte(`{"app": "nfs-client-provisioner"}`)
-		// err := json.Unmarshal(appLabel, &labelOptions)
-		// _, err = clientset.CoreV1().Pods("kube-system").Get("", metav1.GetOptions{labelOptions, "0"})
-
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod not found\n")
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-		} else if err != nil {
+		fetchPod(clientset)
+	} else {
+		clientset, err := internalKubeClient()
+		if err != nil {
 			panic(err.Error())
-		} else {
-			fmt.Printf("Found pod\n")
 		}
-
-		time.Sleep(10 * time.Second)
+		fetchPod(clientset)
 	}
+
 }
