e83b57fc351364a090b3456e5bc94eba04f4dc6f
spirrello <spirrello@opentext.com>
2019-11-19 17:26:47 -0800

    fixed tests

--- services/kcapture-api/main_test.go
+++ services/kcapture-api/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"kcapture/shared"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -16,7 +17,7 @@ func TestHealthCheck(t *testing.T) {
 
 	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(healthCheck)
+	handler := http.HandlerFunc(shared.HealthCheck)
 
 	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
 	// directly and pass in our Request and ResponseRecorder.
